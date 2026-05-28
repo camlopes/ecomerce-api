01bb7c793d0241699c76c4fe78b85fbfc3a4c470
Camila Lopes <camalopes.cat@gmail.com>
2026-05-28 12:39:01 -0300

    Inicio do projeto

---
+++ src/test/java/com/app/ecomerce_api/EcomerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ecomerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
