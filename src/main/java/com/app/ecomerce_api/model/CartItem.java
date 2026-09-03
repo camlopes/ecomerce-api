61a5f4f4d698aa2b96c51f7612228c624f5fa511
Camila Lopes <camalopes.cat@gmail.com>
2026-09-03 16:54:22 -0300

    feat: Add Cart item entity and cart item Repository

---
+++ src/main/java/com/app/ecomerce_api/model/CartItem.java
@@ -0,0 +1,34 @@
+package com.app.ecomerce_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "cart_itens")
+@Data
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
