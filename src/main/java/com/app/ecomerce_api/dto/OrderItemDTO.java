3119faf07056c2921c15daf42fe45a8ce1fc803f
Camila Lopes <camalopes.cat@gmail.com>
2026-09-17 10:42:04 -0300

    fix: remove unnecessary validations

--- src/main/java/com/app/ecomerce_api/dto/OrderItemDTO.java
+++ src/main/java/com/app/ecomerce_api/dto/OrderItemDTO.java
@@ -1,6 +1,5 @@
 package com.app.ecomerce_api.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -8,14 +7,9 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 public class OrderItemDTO {
-    @NotNull
     private Long id;
-    @NotNull
     private Long productId;
-    @NotNull
     private Integer quantity;
-    @NotNull
     private BigDecimal totalPrice;
-    @NotNull
     private BigDecimal productPrice;
 }
